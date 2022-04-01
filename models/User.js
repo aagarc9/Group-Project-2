8381faca8f7d76dd509f3c32428f8427874cfe44
Andrew Ngo <andvngo@gmail.com>
2022-03-31 17:20:39 -0700

    created db\schema.sql; seeds/seed.js with userData.json; added script to seed database to package.json

--- models/User.js
+++ models/User.js
@@ -1,3 +1,49 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+class User extends Model {
+    checkPW(loginPW) {
+        return bcrypt.compareSync(loginPW, this.password);
+    }
+};
 
-class User extends Model {}
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true
+            }
+        },
+    },
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            beforeUpdate: async (updateUserData) => {
+                updateUserData.password = await bcrypt.hash(updateUserData.password, 10);
+                return updateUserData;
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "user",
+    }
+);
+
+module.exports = User
